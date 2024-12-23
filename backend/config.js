addaa8ae7632d8c7da2c50ad746f168c58c72f4f
Askerin <134921117+Cabel7@users.noreply.github.com>
2024-12-24 00:01:32 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://viver:J8pggyspXpeq3cRO@cluster0.uj20r.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://tester:7GtFSCy2XslMpBV6@cluster0.utezo.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0';
 
 // Please create a free database for yourself.
 // This database will be deleted after tutorial
